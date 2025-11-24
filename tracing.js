7c2ea8db97f626d19bcca1c3b49854c63de6d751
Guhan Gnanam <guhan.gnanam@outlook.com>
2025-11-24 18:03:31 -0500

    Final Commit

--- tracing.js
+++ tracing.js
@@ -2,19 +2,17 @@ const { resourceFromAttributes } = require("@opentelemetry/resources");
 const { ATTR_SERVICE_NAME } = require("@opentelemetry/semantic-conventions");
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-
-
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
-
-
 const {
   ConsoleMetricExporter,
   PeriodicExportingMetricReader,
 } = require("@opentelemetry/sdk-metrics");
-
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+
+
+
 
 // send to jaeger now
 const traceExporter = new OTLPTraceExporter({
